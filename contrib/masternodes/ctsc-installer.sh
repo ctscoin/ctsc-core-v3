6fc789af9eae33f097fcf42a95d013c76fea5d59
CTS Coin Team <42897378+ctscoin@users.noreply.github.com>
2019-12-30 14:28:50 +0100

    Fixing install script

--- contrib/masternodes/ctsc-installer.sh
+++ contrib/masternodes/ctsc-installer.sh
@@ -29,7 +29,7 @@ function download_node() {
   echo -e "Preparing to download updated $COIN_NAME binaries..."
   cd $TMP_FOLDER
   wget -q $COIN_TGZ
-  tar xvzf $COIN_ZIP -C /usr/local/bin/ --strip=1
+  tar xvzf $COIN_ZIP -C /usr/local/bin/
   compile_error
   chmod +x $COIN_PATH$COIN_DAEMON $COIN_PATH$COIN_CLI
   cd - >/dev/null 2>&1
