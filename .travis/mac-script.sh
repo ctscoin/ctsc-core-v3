ec518217275aacad036d51e5147b2c2d9b70f187
pbzrpa <pbzrpa@gmail.com>
2020-07-30 00:47:42 +0200

    Updated name of dmg file

--- .travis/mac-script.sh
+++ .travis/mac-script.sh
@@ -6,4 +6,4 @@ export LC_ALL=C.UTF-8
 ./configure LDFLAGS='-L/usr/local/opt/openssl/lib' CPPFLAGS='-I/usr/local/opt/openssl/include' PKG_CONFIG_PATH='/usr/local/opt/openssl/lib/pkgconfig' --with-gui=qt5 --with-boost-system=boost_system-mt --with-boost-filesystem=boost_filesystem-mt --with-boost-thread=boost_thread-mt --with-boost-chrono=boost_chrono-mt --disable-tests
 make -j4
 make deploy
-mv KYD-Qt.dmg $RELEASE_FILE
+mv CTSC-Qt.dmg $RELEASE_FILE
