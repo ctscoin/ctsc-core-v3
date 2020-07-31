3715d2bd7029554e7a4bf9fff819d2cc457dc895
pbzrpa <pbzrpa@gmail.com>
2020-07-31 08:31:08 +0200

    MN Install script update to 3.1.4

--- contrib/masternodes/ctsc-installer.sh
+++ contrib/masternodes/ctsc-installer.sh
@@ -10,9 +10,9 @@ CONFIGFOLDER='/root/.ctsc'
 COIN_DAEMON='ctscd'
 COIN_CLI='ctsc-cli'
 COIN_PATH='/usr/local/bin/'
-COIN_TGZ='https://github.com/ctscoin/ctsc-core-v3/releases/download/v3.1.2/CTSC-v3.1.2-linux-x86_64.tar.gz'
-COIN_BOOTSTRAP='https://staticfiles.ctscoin.net/bootstrap/ctsc-bootstrap-312.zip'
-BOOTSTRAP_ZIP='ctsc-bootstrap-312.zip'
+COIN_TGZ='https://github.com/ctscoin/ctsc-core-v3/releases/download/v3.1.4b/ctsc-v3.1.4b-ubuntu16.04-x86-64.tar.gz'
+COIN_BOOTSTRAP='https://staticfiles.ctscoin.net/bootstrap/ctsc-bootstrap-314.zip'
+BOOTSTRAP_ZIP='ctsc-bootstrap-314.zip'
 #$(echo $COIN_BOOTSTRAP | awk -F'/' '{print $NF}')
 COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 COIN_NAME='CTSC'
